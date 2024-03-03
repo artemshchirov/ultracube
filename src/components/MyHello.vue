583feb602a35dec77278b2f284daea3b7053e25e
Artem Shchirov <artemshchirov@gmail.com>
2024-03-04 00:41:54 +0200

    init

---
+++ src/components/MyHello.vue
@@ -0,0 +1,14 @@
+<script setup>
+alert('hello')
+</script>
+
+<template>
+  <p class="text">My Component</p>
+  <button>Open</button>
+</template>
+
+<style scoped>
+.text {
+  background: palegoldenrod;
+}
+</style>
